b7696b90493005badda7c979360ced1036bbbf82
Nydia Massiel Sánchez <66643813+nydsanchez@users.noreply.github.com>
2024-08-17 10:28:04 -0600

    chore: set up server and create general folders

---
+++ index.js
@@ -0,0 +1,12 @@
+const server = require("./src/server.js");
+const { conn } = require("./src/db.js");
+const PORT = 4000;
+
+conn
+  .sync({ force: false })
+  .then(() => {
+    server.listen(PORT, "0.0.0.0", () => {
+      console.log(`Server listening on port ${PORT}`);
+    });
+  })
+  .catch((error) => console.error(error));
