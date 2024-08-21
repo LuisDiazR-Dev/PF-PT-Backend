bfdb4f005ed733eeb87249d9ce8b330f2bf3de8d
damianmr93 <damianmr93@gmail.com>
2024-08-21 02:35:38 -0300

    creacion de modelos para la db

---
+++ src/models/Suscription.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require('sequelize')
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+	// defino el modelo
+	sequelize.define(
+		'Suscription',
+		{
+			id: {
+				type: DataTypes.INTEGER,
+				autoIncrement: true,
+				primaryKey: true,
+			},
+			plan_price: {
+				dataType: DataTypes.INTEGER,
+				allowNull: false,
+			},
+			registration_date: {
+				type: DataTypes.DATE,
+			},
+		},
+		{
+			timestamps: false,
+		}
+	)
+}
