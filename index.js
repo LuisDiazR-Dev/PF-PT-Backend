6a2226b7982e9eacb7970bdf7bec6d7f9232f738
JGuzman40 <ticguzman78@gmail.com>
2024-08-26 00:51:56 -0500

    Configuracion lista para carga de datos inicial

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ require('dotenv').config()
 const server = require('./src/server.js')
 const { conn } = require('./src/db.js')
 const { PORT } = process.env
+const seedData = require('./src/seeders/seed-data')
 
 conn
 	.sync({ alter: true })
-	.then(() => {
+	.then(async () => {
+		await seedData()
 		server.listen(PORT, () => {
 			console.log(`Server listening on port ${PORT}`)
 		})
