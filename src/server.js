7102e303f837d8bd3d6acfffa9b9c714a088d814
LuisDiazR-Dev <diazr.info@gmail.com>
2024-09-16 18:32:46 -0500

    fix de ajustes de server ya la solicitud al baken desde el frontend se esta bloqueando

--- src/server.js
+++ src/server.js
@@ -5,6 +5,10 @@ const cors = require('cors')
 
 const server = express()
 
+server.use(morgan('dev'))
+server.use(express.json())
+server.use(cors())
+
 server.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*')
 	res.header('Access-Control-Allow-Credentials', 'true')
@@ -15,10 +19,6 @@ server.use((req, res, next) => {
 	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
 	next()
 })
-
-server.use(morgan('dev'))
-server.use(express.json())
-server.use(cors())
 
 server.use('/api', router)
 
