d373b670af383a66060b02a2b90341a6afe9276a
LuisDiazR-Dev <diazr.info@gmail.com>
2024-09-19 23:41:09 -0500

    scope: ['profile', 'email'] en authservice google

--- src/services/authServiceGoogle.js
+++ src/services/authServiceGoogle.js
@@ -7,6 +7,7 @@ passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: process.env.URL_API_GOOGLE,
+    scope: ['profile', 'email']
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
